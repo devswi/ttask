25a5e179087e5ee64a54398d61eef365bed9b217
shiwei93 <stayfocusjs@gmail.com>
2022-12-29 22:13:03 +0800

    添加包分析插件

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,4 +1,5 @@
 const { PUBLIC_PATH } = require('./constants');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { merge } = require('webpack-merge');
 const base = require('./webpack.base');
 
@@ -7,12 +8,16 @@ module.exports = merge(base, {
     output: {
         filename: '[name].bundle.js',
     },
+    plugins: [
+        new BundleAnalyzerPlugin({ openAnalyzer: false, analyzerPort: 8001, logLevel: 'silent' }),
+    ],
+    devtool: 'eval-cheap-module-source-map',
     devServer: {
         static: {
             directory: PUBLIC_PATH,
         },
         allowedHosts: ['local.ttask.com'],
-        port: 8483,
+        port: 8484,
         client: {
             logging: 'info',
             overlay: { warnings: false, errors: true },
